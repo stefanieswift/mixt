54bce0031c2ff725d5e8761312f7a2b5fc402d88
Stefanie Swift <swift.stefanie@gmail.com>
2015-04-13 16:08:37 -0700

    trying to play with jquery

--- public/js/application.js
+++ public/js/application.js
@@ -1,2 +1,7 @@
-
 $(function() { $( ".header_image" ).hide().show( "slow" )});
+
+$(function() { $( ".welcome_video" ).hide()});
+
+$(function() { $( ".header_image" ).click(function(){ $( ".welcome_video" ).show();
+  $( ".header_image" ).hide();
+});});
