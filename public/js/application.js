35297875e32a8bd7dd8485b924daa13c800ac095
Stefanie Swift <swift.stefanie@gmail.com>
2015-04-13 16:10:02 -0700

    deleted video

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,2 @@
 $(function() { $( ".header_image" ).hide().show( "slow" )});
 
-$(function() { $( ".welcome_video" ).hide()});
-
-$(function() { $( ".header_image" ).click(function(){ $( ".welcome_video" ).show();
-  $( ".header_image" ).hide();
-});});
