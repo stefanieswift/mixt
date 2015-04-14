ac489d322104e744a272a22088c82b975a411166
Stefanie Swift <swift.stefanie@gmail.com>
2015-04-13 19:16:26 -0700

    changed my submit form

--- public/js/application.js
+++ public/js/application.js
@@ -12,6 +12,9 @@ $(".nav_menu a").each(function(){
   var $anchor = $(this);
   var $option = $("<option></option>")
 
+  if ($anchor.parent().hasClass("selected")){
+    $option.prop("selected", true);
+  }
   $option.val($anchor.attr("href"));
   $option.text($anchor.text());
   $select.append($option);
@@ -23,3 +26,56 @@ $(".nav_menu").append($button);
 $button.click(function(){
   window.location = $select.val();
 });
+
+// password confirmation feature
+var $password = $("#password");
+var $confirmPassword = $("#confirm_password");
+
+//Hide hints
+$("form span").hide();
+
+function isPasswordValid() {
+  return $password.val().length > 8;
+}
+
+function arePasswordsMatching() {
+  return $password.val() === $confirmPassword.val();
+}
+
+function canSubmit() {
+  return isPasswordValid() && arePasswordsMatching();
+}
+
+function passwordEvent(){
+    //Find out if password is valid
+    if(isPasswordValid()) {
+      //Hide hint if valid
+      $password.next().hide();
+    } else {
+      //else show hint
+      $password.next().show();
+    }
+}
+
+function confirmPasswordEvent() {
+  //Find out if password and confirmation match
+  if(arePasswordsMatching()) {
+    //Hide hint if match
+    $confirmPassword.next().hide();
+  } else {
+    //else show hint
+    $confirmPassword.next().show();
+  }
+}
+
+function enableSubmitEvent() {
+  $("#submit").prop("disabled", !canSubmit());
+}
+
+//When event happens on password input
+$password.focus(passwordEvent).keyup(passwordEvent).keyup(confirmPasswordEvent).keyup(enableSubmitEvent);
+
+//When event happens on confirmation input
+$confirmPassword.focus(confirmPasswordEvent).keyup(confirmPasswordEvent).keyup(enableSubmitEvent);
+
+enableSubmitEvent();
