7092f53ba8d46a7fa5dc4fd6924d8d2eae536fe6
Stefanie Swift <swift.stefanie@gmail.com>
2015-04-13 17:00:05 -0700

    editing it all back# Please enter the commit message for your changes. Lines starting

--- public/js/application.js
+++ public/js/application.js
@@ -1,10 +1,14 @@
-$( ".header_image" ).hide().show( "slow" );
+$(function() { $( ".header_image" ).hide().show( "slow" )});
 
+
+// nav_menu
+
+// login_menu
 
 // var $select = $( "<select></select>");
-// $(".nav_menu").append($select);
+// $(".login_menu").append($select);
 
-// $(".nav_menu a").each(function(){
+// $(".login_menu a").each(function(){
 //   var $anchor = $(this);
 //   var $option = $("<option></option>")
 
@@ -14,7 +18,7 @@ $( ".header_image" ).hide().show( "slow" );
 // });
 
 // var $button = $("<button>Go</button>");
-// $(".nav_menu").append($button);
+// $(".login_menu").append($button);
 
 // $button.click(function(){
 //   window.location = $select.val();
